00329e20fe83b1405fb6144c30b01bb3219cdf38
Rifqiwar <r.wariyanto@gmail.com>
2020-02-21 15:48:45 +0700

    Belajar Witged

--- novice/02-02/kasus/handbook/lib/main.dart
+++ novice/02-02/kasus/handbook/lib/main.dart
@@ -98,56 +98,230 @@
 //   }
 // }
 
+
+// import 'package:flutter/material.dart';
+
+// void main(){
+//   runApp(new MaterialApp(
+//     home: new MyApp()
+//   ));
+
+  
+// }
+
+// class MyApp extends StatefulWidget{
+//   @override
+//   MyAppState createState() => new MyAppState();
+
+// }
+
+// class MyAppState extends State<MyApp>{
+//   int counter = 0;
+//   List <String> string = ["Flutter","Is", "Awesome"];
+//   String displayedString = "";
+
+//   void onPressed(){
+//     setState(() {
+//       displayedString = string[counter];
+//       counter = counter < 2 ? counter +1 :0;
+//     });
+//   }
+
+
+// @override 
+// Widget build (BuildContext context){
+//     return new Scaffold(
+//       appBar: new AppBar(title: new Text("Statefull Widget"), backgroundColor: Colors.orange),
+//       body: new Container(
+//         child: new Center(
+//           child: new Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+//             new Padding(padding: new EdgeInsets.all(15.0)),
+//             new RaisedButton(
+//               child: new Text("Press me!", style: new TextStyle(color:  Colors.white, fontStyle: FontStyle.italic, fontSize:20.0)),
+//               color: Colors.red,
+//               onPressed: onPressed
+//             )
+//           ]
+//         )
+//       )
+//     )
+//   );
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+
+// void main(){
+//   runApp(new MaterialApp(
+//     home:  new MyApp(),
+//     routes: <String, WidgetBuilder>{
+//       "/SecondPage":(BuildContext context) => new SecondPage()
+//     },
+//   ));
+// }
+
+// class MyApp extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context){
+//     return new Scaffold(
+//       appBar: new AppBar(title: new Text('HomePage'), backgroundColor: Colors.tealAccent),
+//       body: new Container(
+//         child: new Center(
+//           child: new Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               new IconButton(
+//                 icon: new Icon(Icons.favorite, color:Colors.redAccent),
+//                 iconSize: 70.0,
+//                 onPressed: (){ Navigator.of(context).pushNamed("/SecondPage");},
+
+//               ),
+//               new Text("Home")
+//             ]
+//           )
+//         )
+//       ),
+//     );
+//   }
+// }
+
+// class SecondPage extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context){
+//     return new Scaffold(
+//       appBar: new AppBar(title: new Text ("Second Page"), backgroundColor: Colors.pink),
+//       body: new Container(
+//         child: Center(
+//           child: new Column(
+//             mainAxisAlignment : MainAxisAlignment.center,
+//             children: <Widget>[
+//               new IconButton(
+//                 icon: new Icon(Icons.file_download, color:Colors.brown),
+//                 iconSize: 20.0,
+//                 onPressed: null,
+//               ),
+//               new Text("Second Page")
+//             ]
+//           )
+//         )
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp()
-  ));
-}
+void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  AwesomeButtonState createState() => new AwesomeButtonState();
-}
-
-class AwesomeButtonState extends State<MyApp> {
-
-  int counter = 0;
-  List<String> strings = ["Flutter", "Is", "Awesome"];
-  String displayedString = "";
-
-  //you can also declare variables like this
-  // var counter = 0;
-  // var strings = ["Flutter", "Is", "Awesome"];
-
-  void onPressed(){
-    setState(() {
-      displayedString = strings[counter];
-      counter = counter < 2 ? counter+1 : 0;
-    });
-  }
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("Stateful Widget!"), backgroundColor: Colors.deepOrange),
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(displayedString, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
-              new Padding(padding: new EdgeInsets.all(15.0)),
-              new RaisedButton(
-                child: new Text("Press me!", style: new TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 20.0)),
-                color: Colors.red,
-                onPressed: onPressed
-              )
-            ]
-          )
-        )
-      )
+    var routes = <String, WidgetBuilder>{
+      MyItemsPage.routeName: (BuildContext context) => new MyItemsPage(title: "MyItemPage"),
+
+    };
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: new MyHomePage(title: 'Fluuter Home Page'),
+      routes: routes,
     );
   }
 }
+
+class MyHomePage extends StatefulWidget{
+  MyHomePage({Key key, this.title}) : super(key:key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState()=> new _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage>{
+int _counter = 0;
+
+void _incrementCounter(){
+  Navigator.pushNamed(context, MyItemsPage.routeName);
+}
+
+@override
+Widget build(BuildContext context){
+    var button = new IconButton(icon: new Icon(Icons.arrow_back), onPressed: _onButtonPressed);
+  return new Scaffold(
+    appBar: new AppBar(
+      title: new Text (widget.title),
+    ),
+    body: new Column(
+      children: <Widget>[
+        new Text ('Kamu'),
+        new Text ('Aku'),
+        button
+      ],
+    ),
+    floatingActionButton: new FloatingActionButton(
+      onPressed: _incrementCounter,
+      tooltip:'increment',
+      child: new Icon(Icons.add),
+    ),
+  );
+}
+
+
+void _onButtonPressed(){
+  Navigator.pushNamed(context, MyItemsPage.routeName);
+
+}
+}
+
+class MyItemsPage extends StatefulWidget{
+  MyItemsPage({Key key, this.title}):super(key:key);
+
+  static const String routeName = "/MyItemPage";
+
+  final String title;
+
+  @override
+  _MyItemsPageState createState() => new _MyItemsPageState();
+}
+
+class _MyItemsPageState extends State<MyItemsPage>{
+  @override
+  Widget build(BuildContext context){
+    var button = new IconButton(icon: new Icon(Icons.arrow_back), onPressed: _onButtonPressed);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Container(
+        child: new Column(
+          children:<Widget>[
+            new Text('Item 1'),
+            new Text('Item 2'),
+            button
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(onPressed: _onFloatingActionButtonPressed,
+      tooltip:'Add',
+      child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _onFloatingActionButtonPressed(){
+
+  }
+
+  void _onButtonPressed(){
+    Navigator.pop(context);
+  }
+}
+
--- novice/02-02/kasus/handbook/test/widget_test.dart
+++ novice/02-02/kasus/handbook/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:handbook/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(AwesomeButton());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
